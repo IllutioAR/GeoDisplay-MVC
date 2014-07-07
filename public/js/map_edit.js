bb4eb86c34dcf0f278640eb0331f9ecdfd6ddc15
Antonio <antonio.hdez93@gmail.com>
2014-07-07 18:47:14 -0500

    Optimización de Script de Google Maps.
    - Se eliminó la parte de geolocalización en el mapa para editar
    posición.

--- public/js/map_edit.js
+++ public/js/map_edit.js
@@ -11,23 +11,17 @@ function initialize() {
 	map = new google.maps.Map(document.getElementById("map"),
 		mapOptions);
 
-	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			initialLocation = new google.maps.LatLng(document.getElementById('latitude').value, document.getElementById('longitude').value);
-			map.setCenter(initialLocation);
-			marker = new google.maps.Marker({
-			    position: initialLocation,
-			    draggable: true,
-			    animation: google.maps.Animation.DROP
-			});
-			marker.setMap(map);
-			google.maps.event.addListener(marker, 'mouseup', function() {
-				setPositionForm();
-			});
-		}, function() {
-			
-		});
-	}
+	initialLocation = new google.maps.LatLng(document.getElementById('latitude').value, document.getElementById('longitude').value);
+	map.setCenter(initialLocation);
+	marker = new google.maps.Marker({
+	    position: initialLocation,
+	    draggable: true,
+	    animation: google.maps.Animation.DROP
+	});
+	marker.setMap(map);
+	google.maps.event.addListener(marker, 'mouseup', function() {
+		setPositionForm();
+	});
 
 }
 
